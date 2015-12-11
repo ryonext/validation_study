2fbaf2097578f5608eaf9042bec6ea322120cac7
ryonext <ryonext.s@gmail.com>
2015-12-11 22:42:22 +0900

    Do migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203132301) do
+ActiveRecord::Schema.define(version: 20151211105430) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "user_validate_only_dbs", force: :cascade do |t|
     t.string   "name",       limit: 20

bdf9cc5a55ea2c308820d27479b6fdb778814677
ryonext <ryonext.s@gmail.com>
2015-12-11 22:56:56 +0900

    Fix account definition.

--- db/migrate/20151211105430_create_accounts.rb
+++ db/migrate/20151211105430_create_accounts.rb
@@ -1,9 +1,11 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
-      t.string :username
+      t.string :username, limit: 20, null: false
 
       t.timestamps null: false
     end
+
+    add_index :accounts, :username, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,12 @@
 ActiveRecord::Schema.define(version: 20151211105430) do
 
   create_table "accounts", force: :cascade do |t|
-    t.string   "username",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "username",   limit: 20, null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
+
+  add_index "accounts", ["username"], name: "index_accounts_on_username", unique: true, using: :btree
 
   create_table "user_validate_only_dbs", force: :cascade do |t|
     t.string   "name",       limit: 20

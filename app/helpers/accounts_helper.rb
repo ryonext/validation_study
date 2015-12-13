f18a1a7e19bcc3c8ef1449f09143c61f0135ad9f
ryonext <ryonext.s@gmail.com>
2015-12-13 11:40:53 +0900

    Fix error.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,11 +1,11 @@
 module AccountsHelper
   def error_message(count)
-    if count == 0
+    if count == 1
       "このユーザ名はすでに使われています。"
-    elsif count == 1
+    elsif count == 2
       "このユーザ名も使われています。m9(^Д^)ﾌﾟｷﾞｬｰ"
     else
-      "このユーザ名も使われていますwww<br>#{image_tag 'donna_kimochi.png'}"
+      "このユーザ名も使われてまーすwww<br>#{image_tag 'donna_kimochi.png'}"
     end
   end
 end
